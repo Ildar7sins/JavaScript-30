10ae4be4a47c4cc4c039175ffcfab0136a1d49ee
Ildar <Ildar7sins@gmail.com>
2017-11-05 16:08:57 +0500

    Add second day

---
+++ #2/script.js
@@ -0,0 +1,20 @@
+const secondHand = document.querySelector('.clock__hand--second');
+const minutesHand = document.querySelector('.clock__hand--minute');
+const hoursHand = document.querySelector('.clock__hand--hour');
+
+function setDate(){
+	const now = new Date();
+	const second = now.getSeconds();
+	const minutes = now.getMinutes() ;
+	const hours = now.getHours() ;
+console.log(hours + 1);
+	const secondDegrees = (( second / 60) * 360) + 90;
+	const minutesDegrees = (( minutes / 60) * 360) + 90;
+	const hoursDegrees = (( hours / 60) * 360) + 90;
+
+	secondHand.style.transform= `rotate(${secondDegrees}deg)`;
+	minutesHand.style.transform= `rotate(${minutesDegrees}deg)`;
+	hoursHand.style.transform= `rotate(${hoursDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
